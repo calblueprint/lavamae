e743d989eb15c376699fcd4e683a28acaed896c1
Casey Takahashi <caseytaka@berkeley.edu>
2016-10-02 15:01:57 -0700

    Devise attempts

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002200608) do
+ActiveRecord::Schema.define(version: 20161002210825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
