8c7088b7386137c68f23cf95216e74e5481c7745
Jason Xie <jason.xie@berkeley.edu>
2016-10-02 12:56:18 -0700

    merged discussion with responses

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,4 @@
 class Response < ActiveRecord::Base
 	# belongs_to :user (TODO) Uncomment after User model is implemented
-	# belongs_to :discussion (TODO) Uncomment after Discussion model is implemented
+	belongs_to :discussion
 end
