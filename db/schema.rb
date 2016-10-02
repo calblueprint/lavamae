c828304774aba5240ccab4a6424df79b302b6e92
Jason Xie <jason.xie@berkeley.edu>
2016-10-02 12:51:04 -0700

    added response model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161002192424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "responses", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "score"
+    t.integer  "discussion_id"
+    t.boolean  "is_admin"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
---
+++ spec/factories/responses.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :response do
+    content "MyText"
+    score 1
+    discussion_id 1
+  end
+end
