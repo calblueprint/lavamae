d2b803d4a6cd9d11f19cf8463354cafc187dbbf0
Lois Chang <loischang@berkeley.edu>
2016-10-06 15:34:41 -0700

    discussions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   resources :responses
+  resources :discussions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
