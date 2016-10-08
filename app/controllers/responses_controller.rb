2163da4f37e8b6b3e12cd6c103dcf8cfed2d44ed
Jason Xie <jason.xie@berkeley.edu>
2016-10-08 14:15:16 -0700

    added discussions and nested responses under discussions

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,43 +1,46 @@
 class ResponsesController < ApplicationController
 
-    before_action :get_response, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @responses = Response.all
-  end
+  before_action :get_response, only: [:show, :edit, :update, :destroy]
+  before_action :get_discussion, only: [:create, :destroy]
 
   def create
     response = Response.new(response_params)
-    response.save
-    redirect_to responses_path
+    response.discussion_id = @discussion.id
+    if response.save
+      redirect_to discussion_path(@discussion)
+    else
+      redirect_to root_path
+    end
   end
 
   def new
     # nothing here
   end
 
-  def show 
-    # work done in get_response
-  end
-
   def edit
+  
   end
 
   def update
+  
   end
 
   def destroy
     @response.destroy
-    redirect_to responses_path
+    redirect_to discussion_path(@discussion)
   end
 
   private
     def response_params
-        params.require(:response).permit(:content)
+      params.require(:response).permit(:content)
     end
 
     def get_response
-        @response = Response.find(params[:id])
+      @response = Response.find(params[:id])
+    end
+
+    def get_discussion
+      @discussion = Discussion.find(params[:discussion_id])
     end
 
 end
