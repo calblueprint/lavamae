a1d96552659ea4a8b6011b8703436810963e0fec
Lois Chang <loischang@berkeley.edu>
2016-10-16 12:05:44 -0700

    edit/update discussion

---
+++ app/controllers/discussions_controller.rb
@@ -0,0 +1,47 @@
+class DiscussionsController < ApplicationController
+
+  before_action :get_discussion, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@discussion = Discussion.all.sort_by &:created_at
+  end
+
+  def create
+  	discussion = Discussion.new(discussion_params)
+  	discussion.save
+  	redirect_to discussions_path
+  end
+
+  def new
+  	@discussion = Discussion.new
+  end
+
+  def show
+  	# called in before_action
+  end
+
+  def edit
+  	# same concept as new/create
+  end
+
+  def update
+  	@discussion.update(discussion_params)
+  	redirect_to discussions_path
+  end
+
+  def destroy
+  	#called in before_action
+  	@discussion.destroy
+  	redirect_to discussions_path
+  end
+
+  private
+  	def discussion_params
+  		params.require(:discussion).permit(:title, :content)  # return {content: "stuff", title: "stuff"}
+  	end
+
+  	def get_discussion
+  		@discussion = Discussion.find(params[:id])
+  	end
+
+end
