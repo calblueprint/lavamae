70249aeb86123db215eb642ba757a160335214a2
Jason Xie <jason.xie@berkeley.edu>
2016-10-16 14:06:40 -0700

    updated the views to share css style from master, and made update method for responses

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -25,9 +25,11 @@ class DiscussionsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    
   end
 
   def destroy
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,4 +1,4 @@
 class Discussion < ActiveRecord::Base
 	# belongs_to :user
-	has_many :responses
+	has_many :responses, :dependent => :destroy
 end
