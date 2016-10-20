40d8774f2375f2d227bf32dafa666ab793464be8
Jason Xie <jason.xie@berkeley.edu>
2016-10-19 17:47:52 -0700

    added some user validation to response CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014094114) do
+ActiveRecord::Schema.define(version: 20161019233753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161014094114) do
     t.boolean  "is_admin"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
