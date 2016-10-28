1fda0d5b217ce5671fa5cd05acf8f3a37828299a
Casey Takahashi <caseytaka@berkeley.edu>
2016-10-28 11:04:15 -0700

    Edit routing works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+  end
+
+  def edit
   end
 end
