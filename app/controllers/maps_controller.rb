0451d4a0a04636cad63185e1b90e92eddedd7c09
Andrew Millman <andrewmillman35@gmail.com>
2016-11-30 19:09:12 -0800

    Implement popups and locations on the map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,5 @@
 class MapsController < ApplicationController
   def map
-    @users_on_map = User.where(on_map: true)
+    @users_on_map = User.where(on_map: true).where.not(location_id: nil)
   end
 end
