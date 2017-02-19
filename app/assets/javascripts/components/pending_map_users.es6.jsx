e2cf8e2a54cd11494a268502bac59e6a6e6dac5a
Amy Shan <amyshan@berkeley.edu>
2017-02-19 14:55:04 -0800

    batch update for map approval implemented but has the id=controller bug

--- app/assets/javascripts/components/pending_map_users.es6.jsx
+++ app/assets/javascripts/components/pending_map_users.es6.jsx
@@ -7,23 +7,32 @@ class PendingMapUsers extends React.Component {
     super(props);
     this._handleApprove = this._handleApprove.bind(this);
     this._handleReject = this._handleReject.bind(this);
+    this._handleUpdate = this._handleUpdate.bind(this);
     this.state = {
       users: this.props.pending_users,
-      approved_users: {},
-      rejected_users: {},
+      modified_users: {},
     }
   }
 
   _handleApprove(user) {
-    this.state.approved_users[user.id] = {map_approval_state: 1}
+    new_modified_users = this.state.modified_users
+    new_modified_users[user.id] = {map_approval_state: 'approved'}
+    this.setState({modified_users: new_modified_users})
   }
 
   _handleReject(user) {
-    this.state.rejected_users[user.id] = {map_approval_state: 2}
+    new_modified_users = this.state.modified_users
+    new_modified_users[user.id] = {map_approval_state: 'rejected'}
+    this.setState({modified_users: new_modified_users})
+  }
+
+  _success(msg) {
+    toastr.success("Update successful!");
+    window.location = location.pathname;
   }
 
   _handleUpdate() {
-
+    APIRequester.put('/users/update_map_approval', this.state.modified_users, this._success);
   }
 
   _renderUsers() {
@@ -46,7 +55,7 @@ class PendingMapUsers extends React.Component {
   render() {
     return (
       <div>
-        "Users Pending Map Approval"
+        Users Pending Map Approval
         {this._renderUsers()}
         <button className="btn btn-outline" onClick={this._handleUpdate}>Update</button>
       </div>
