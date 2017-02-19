9820a14c14feb4ec92c3c0c58a2dbc911c3b8418
Amy Shan <amyshan@berkeley.edu>
2017-02-19 13:40:21 -0800

    map_approval_state attribute added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     end
     @favorite_discussions = @user.favorite_discussions
     if @user.is_admin
-      @pending_map_users = User.where(map_approved: false, on_map: true)
+      @pending_map_users = User.where(map_approval_state: 0, on_map: true)
     else
       @pending_map_users = []
     end
