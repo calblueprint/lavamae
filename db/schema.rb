9d9f1fd76d10e65a12ce8bb3e5405fc199783db8
Amy Shan <amyshan@berkeley.edu>
2017-02-19 01:25:09 -0800

    created basic pending_map_users component to display relevant users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128083310) do
+ActiveRecord::Schema.define(version: 20170216032611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,20 +110,21 @@ ActiveRecord::Schema.define(version: 20161128083310) do
     t.integer  "num_actions"
     t.boolean  "is_admin"
     t.boolean  "on_map"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: "",    null: false
     t.string   "profile_pic"
     t.integer  "location_id"
+    t.boolean  "map_approved",           default: false
   end
 
   add_index "users", ["location_id"], name: "index_users_on_location_id", using: :btree
