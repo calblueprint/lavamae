0171f68a47ee1e8a77fff03838a0c0ee628be729
Amy Shan <amyshan@berkeley.edu>
2017-02-19 13:40:21 -0800

    map_approval_state attribute added

--- app/assets/javascripts/components/pending_map_users.es6.jsx
+++ app/assets/javascripts/components/pending_map_users.es6.jsx
@@ -5,23 +5,56 @@
 class PendingMapUsers extends React.Component {
   constructor(props) {
     super(props);
+    this._handleApprove = this._handleApprove.bind(this);
+    this._handleReject = this._handleReject.bind(this);
     this.state = {
       users: this.props.pending_users,
+      approved_users: {},
+      rejected_users: {},
     }
   }
 
-  render() {
-    console.log(this.state.users);
-    return (
+  _handleApprove(user) {
+    this.state.approved_users[user.id] = {map_approval_state: 1}
+  }
 
+  _handleReject(user) {
+    this.state.rejected_users[user.id] = {map_approval_state: 2}
+  }
+
+  _handleUpdate() {
+
+  }
+
+  _renderUsers() {
+    if (this.state.users == undefined) {
+      return
+    }
+
+    return this.state.users.map((pending_user) => {
+      return (
+        <ApproveUser
+          key = {pending_user.id}
+          pending_user = {pending_user}
+          _handleApprove = {this._handleApprove}
+          _handleReject = {this._handleReject}
+        />
+      )
+    });
+  }
+
+  render() {
+    return (
       <div>
-        "hello"
+        "Users Pending Map Approval"
+        {this._renderUsers()}
+        <button className="btn btn-outline" onClick={this._handleUpdate}>Update</button>
       </div>
     );
   }
 
 }
-
+1
 PendingMapUsers.propTypes = {
   pending_users: React.PropTypes.array.isRequired,
 }
