25d966c502b69a719077bbd4460979a2b2ae9f8e
Casey Takahashi <caseytaka@berkeley.edu>
2017-02-19 15:55:16 -0800

    make seed data for users and admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def make_users
+  1.upto(10) do |n|
+    user = User.create(
+      first_name: FFaker::Name.first_name,
+      last_name: FFaker::Name.last_name,
+      is_admin: false,
+    )
+    user.id = n
+    user.save
+  end
+end
+
+def make_admins
+  1.upto(10) do |n|
+    admin = User.create(
+      first_name: FFaker::Name.first_name,
+      last_name: FFaker::Name.last_name,
+      is_admin: true,
+    )
+    admin.id = n
+    admin.save
+  end
+end
+
+make_users
+make_admins
