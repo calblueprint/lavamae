9e4eb11a678dbac40ac93b4a103ec6952b4370f1
Nhi Quach <nhiquach@berkeley.edu>
2017-02-26 01:09:21 -0800

    discussion page react conversion WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,7 +49,8 @@ def make_discussions
       content: "lavabae++",
       title: "Discussion #{n}",
       tag_list: ["Volunteering", "Starting up"],
-      user_id: n % 5 + 1
+      user_id: n % 5 + 1,
+      score: 0
     )
     discussion.score = 0
     discussion.upvotes = []
