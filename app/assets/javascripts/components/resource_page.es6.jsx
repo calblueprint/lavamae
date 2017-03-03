f490e791bf4da9de6c2efb14e7935a49bbcef7f7
Casey Takahashi <caseytaka@berkeley.edu>
2017-03-02 20:29:59 -0800

    only admins can update, edit, and delete resources

--- app/assets/javascripts/components/resource_page.es6.jsx
+++ app/assets/javascripts/components/resource_page.es6.jsx
@@ -1,9 +1,15 @@
+/**
+  * @props is_admin - true if current user is an admin
+  */
+
 class ResourcePage extends React.Component {
   constructor(props) {
     super(props);
     this._renderModule = this._renderModule.bind(this);
+    this._renderModules = this._renderModules.bind(this);
     this._success = this._success.bind(this);
     this._fetchModules = this._fetchModules.bind(this);
+    this._renderModuleUploadModal = this._renderModuleUploadModal.bind(this);
     this.state = {
       modules : []
     };
@@ -16,8 +22,9 @@ class ResourcePage extends React.Component {
   _renderModule(resource_topic) {
     return (
       <ResourceModule
-        key={resource_topic.id}
-        resource_topic = {resource_topic}
+        key = { resource_topic.id }
+        resource_topic = { resource_topic }
+        is_admin = { this.props.is_admin }
       />
     );
   }
@@ -34,13 +41,23 @@ class ResourcePage extends React.Component {
     return this.state.modules.map((resource_topic) => this._renderModule(resource_topic));
   }
 
+  _renderModuleUploadModal() {
+    let moduleUploadModal;
+    if (this.props.is_admin) {
+      moduleUploadModal = (
+        <ModuleUploadModal style = { "btn-btn-blue" } />
+      );
+    }
+    return moduleUploadModal;
+  }
+
   render() {
     return (
       <section>
         <div className="container modules-container">
           <div className="modules-header">
             <h3>Modules</h3>
-            <ModuleUploadModal style = { "btn-btn-blue" } />
+            { this._renderModuleUploadModal() }
           </div>
           <div className="modules row">
             {this._renderModules()}
@@ -50,3 +67,7 @@ class ResourcePage extends React.Component {
     )
   }
 }
+
+ResourcePage.propTypes = {
+  is_admin: React.PropTypes.bool.isRequired,
+};
