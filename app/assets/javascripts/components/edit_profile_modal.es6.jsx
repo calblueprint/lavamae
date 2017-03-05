2a855c85963b2aa4dcf5fb133929f4417b319b5d
Amy Shan <amyshan@berkeley.edu>
2017-03-05 13:55:30 -0800

    finished implementing edit profile pic

--- app/assets/javascripts/components/edit_profile_modal.es6.jsx
+++ app/assets/javascripts/components/edit_profile_modal.es6.jsx
@@ -29,6 +29,7 @@ class EditProfileModal extends React.Component {
     this._handleFileChange = this._handleFileChange.bind(this);
     this.state = {
       showModal: false,
+      // TODO (amy): fix nested profile_pic prop
       profile_pic: this.props.profile_pic,
       imagePreviewUrl: this.props.profile_pic.profile_pic.url,
       first_name: this.props.first_name,
@@ -166,7 +167,6 @@ class EditProfileModal extends React.Component {
   }
 
   render () {
-    // TODO (amy): fix nested profile_pic prop
     let imagePreviewUrl = this.state.imagePreviewUrl;
     let $imagePreview = null;
     if (imagePreviewUrl) {
