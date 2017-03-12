51bb8cfa51045e175eb8aec8cc2098495c1fc855
Nhi Quach <nhiquach@berkeley.edu>
2017-03-11 22:59:18 -0800

    filter array for multiple tagging

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -13,7 +13,7 @@ class DiscussionsController < ApplicationController
     end
 
   	@discussions = @discussions.search(params[:search]).order('created_at DESC')
-    @discussions = @discussions.tagged_with(params[:filter]) if params[:filter].present?
+    @discussions = @discussions.tagged_with(params[:filter], any: true) if params[:filter].present?
 
     if params[:discussion_id]
       @discussion = Discussion.find(params[:discussion_id])
