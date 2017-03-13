554cc7b5d4be6cf38466135ca49cde0d76780a9c
Lois Chang <loischang@berkeley.edu>
2017-03-12 22:48:15 -0700

    fixed bug

--- app/assets/javascripts/components/upvote.es6.jsx
+++ app/assets/javascripts/components/upvote.es6.jsx
@@ -15,10 +15,9 @@ class Upvote extends React.Component {
       has_upvoted: false,
       response: this.props.response
     };
-
     if (this.props.user && this.props.upvotes.length != 0) {
-      this.state.has_upvoted = this.props.upvotes.every((element) => {
-        element != this.props.user.id
+      this.state.has_upvoted = !this.props.upvotes.every((element) => {
+        element.user_id != this.props.user.id
       })
     }
   }
