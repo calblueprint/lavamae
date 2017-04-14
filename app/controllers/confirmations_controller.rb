eda21d10e35423f9b4f93e6c7d9cb1a0b2464f4d
Lois Chang <loischang@berkeley.edu>
2017-04-13 23:20:33 -0700

    confirmation email now links to home page + opens login modal

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    registration_login_path
+    root_path(:modal => true)
   end
 end
