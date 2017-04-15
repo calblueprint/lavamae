b6badc3ebb6d344df8ba0cd125fc8c9c81488640
gracesnoh <gracesnoh@gmail.com>
2017-04-15 15:00:48 -0700

    volunteer badges

--- app/assets/javascripts/components/volunteer_matching.es6.jsx
+++ app/assets/javascripts/components/volunteer_matching.es6.jsx
@@ -35,12 +35,39 @@ class VolunteerMatching extends React.Component {
       return
     }
     return this.state.volunteers.map((volunteer) => {
-      if (volunteer.volunteer == true ) {
+      if (volunteer.volunteer == true && volunteer.seeking_volunteer == true) {
         return (
           <div key = {volunteer.id} className="user-container volunteer-container">
             <a href={volunteer.id}>
               <div className="user-picture">
-                <img src={volunteer.user_image} />
+                <img src={this.state.profilePic} />
+              </div>
+              <div className="name-date">
+                <div className="user-name">
+                  { volunteer.first_name } { volunteer.last_name }
+                </div>
+              </div>
+              <div className="tooltip">
+                <img className="badge calling-badge" src="/assets/calling-badge.png"></img>
+                <span className="tooltiptext">
+                  Seeking Volunteers
+                </span>
+              </div>
+              <div className="tooltip">
+                <img className="badge" src="/assets/volunteer-badge.png"></img>
+                <span className="tooltiptext">
+                  Volunteer
+                </span>
+              </div>
+            </a>
+          </div>
+        )
+      } else if (volunteer.volunteer == true ) {
+        return (
+          <div key = {volunteer.id} className="user-container volunteer-container">
+            <a href={volunteer.id}>
+              <div className="user-picture">
+                <img src={this.state.profilePic} />
               </div>
               <div className="name-date">
                 <div className="user-name">
@@ -58,14 +85,24 @@ class VolunteerMatching extends React.Component {
         )
       } else if (volunteer.seeking_volunteer == true) {
         return (
-          <div key = {volunteer.id}>
-          <a href={volunteer.id}>
-            <p>
-              { volunteer.first_name } { volunteer.last_name }
-            </p>
-            <p>Looking for volunteers</p>
-          </a>
-        </div>
+          <div key = {volunteer.id} className="user-container volunteer-container">
+            <a href={volunteer.id}>
+              <div className="user-picture">
+                <img src={this.state.profilePic} />
+              </div>
+              <div className="name-date">
+                <div className="user-name">
+                  { volunteer.first_name } { volunteer.last_name }
+                </div>
+              </div>
+              <div className="tooltip">
+                <img className="badge" src="/assets/calling-badge.png"></img>
+                <span className="tooltiptext">
+                  Seeking Volunteers
+                </span>
+              </div>
+            </a>
+          </div>
         )
       }
     });
