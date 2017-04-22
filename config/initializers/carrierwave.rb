ce52b1c79904d673d2f9205689d313693488a953
Casey Takahashi <caseytaka@berkeley.edu>
2017-04-22 14:45:45 -0700

    use aws region environment variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => 'us-west-1'
+    :region                => ENV['S3_REGION']
   }
 
   # For testing, upload files to local `tmp` folder.
